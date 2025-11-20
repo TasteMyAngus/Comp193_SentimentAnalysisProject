df35e715f034a4bcc4d20cb8c1b9ff5fc37ab545
TasteMyAngus <j_glasgow@u.pacific.edu>
2025-11-20 15:16:59 -0800

    Pushed Initialclass changes and tsv

--- InitialClass.py
+++ InitialClass.py
@@ -1,5 +1,23 @@
-import nltk.tokenize import sent_tokenize
+from nltk.tokenize import sent_tokenize
+import nltk
 
 class SentimentAnalyzer:
     def __init__(self):
-        
+        self.self = self
+        nltk.download('punkt')
+
+
+    def text_tokenize(self, text):
+        sentences = sent_tokenize(text)
+        return sentences
+    
+    def clean_sentence(self, sentence):
+        cleaned_sentence = sentence.replace
+    
+    def sentence_tokenize(self, sentence):
+        words = nltk.word_tokenize(sentence)
+        return words
+    
+
+def main():
+    
