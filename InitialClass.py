c7b915af552f6289b009926518b65d5e0402987b
Teran Upchurch <teraniowaupchurch@gmail.com>
2025-11-20 15:57:34 -0800

    Reviews gets column with split senteces and with split words

--- InitialClass.py
+++ InitialClass.py
@@ -3,26 +3,36 @@
 import nltk
 import pandas as pd
 
+nltk.download("punkt_tab")
+
 reviews = pd.read_csv("Restaurant_Reviews.tsv", sep="\t")
+
+
+def text_tokenize(text):
+    # sentences = nltk.sent_tokenize(text)
+    return nltk.sent_tokenize(text)
+
+def clean_sentence(sentence):
+    cleaned_sentence = sentence.replace
+
+def sentence_tokenize(sentence):
+    words = nltk.word_tokenize(sentence)
+    return words
+    
+
+print("Before Anything")
 print(reviews.head())
+print("\n\n")
 
-class SentimentAnalyzer:
-    def __init__(self):
-        self.self = self
-        nltk.download('punkt')
+reviews["Sentences"] = reviews["Review"].apply(text_tokenize)
+print("After Sentence split")
+print(reviews.head())
+print("\n\n")
 
+reviews["Words"] = reviews["Sentences"].apply(
+    lambda sent_list: [sentence_tokenize(s) for s in sent_list]
+)
 
-    def text_tokenize(self, text):
-        sentences = nltk.sent_tokenize(text)
-        return sentences
-    
-    def clean_sentence(self, sentence):
-        cleaned_sentence = sentence.replace
-    
-    def sentence_tokenize(self, sentence):
-        words = nltk.word_tokenize(sentence)
-        return words
-    
-
-#def main():
-    
+print("After Word split")
+print(reviews[0:1])
+print("\n\n")
