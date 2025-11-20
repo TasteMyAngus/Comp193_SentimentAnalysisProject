3af6bbce3d73bab67da70de3603ef938f408ffd9
TasteMyAngus <j_glasgow@u.pacific.edu>
2025-11-20 15:22:14 -0800

    stuff

--- InitialClass.py
+++ InitialClass.py
@@ -1,4 +1,5 @@
-from nltk.tokenize import sent_tokenize
+# From nltk library, we will use word_tokenize and sent_tokenize functions
+
 import nltk
 
 class SentimentAnalyzer:
@@ -8,7 +9,7 @@ class SentimentAnalyzer:
 
 
     def text_tokenize(self, text):
-        sentences = sent_tokenize(text)
+        sentences = nltk.sent_tokenize(text)
         return sentences
     
     def clean_sentence(self, sentence):
