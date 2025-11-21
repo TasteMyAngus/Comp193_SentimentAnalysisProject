d047098693172838e1d70f1e0ad0a7d42e4a2b6e
Teran Upchurch <teraniowaupchurch@gmail.com>
2025-11-20 18:53:44 -0800

    All words go to lower case, some changes for clarity

--- InitialClass.py
+++ InitialClass.py
@@ -7,32 +7,35 @@ nltk.download("punkt_tab")
 
 reviews = pd.read_csv("Restaurant_Reviews.tsv", sep="\t")
 
-
-def text_tokenize(text):
-    # sentences = nltk.sent_tokenize(text)
+def split_into_sentences(text):
     return nltk.sent_tokenize(text)
 
-def clean_sentence(sentence):
-    cleaned_sentence = sentence.replace
+def split_sentences_into_words(sentence):
+    return nltk.word_tokenize(sentence)
 
-def sentence_tokenize(sentence):
-    words = nltk.word_tokenize(sentence)
-    return words
+def lowercase_words(sentence):
+    cleaned_sentence = [word.lower() for word in sentence]
+    return cleaned_sentence
     
+# Print first two lines of data before any changes
+print("\nDefault Data:")
+print(reviews[0:2])
 
-print("Before Anything")
-print(reviews.head())
-print("\n\n")
+# Split text into sentences and print first two lines of data
+reviews["Sentences"] = reviews["Review"].apply(split_into_sentences)
+print("\nAfter Splitting Into Sentences:")
+print(reviews[0:2])
 
-reviews["Sentences"] = reviews["Review"].apply(text_tokenize)
-print("After Sentence split")
-print(reviews.head())
-print("\n\n")
-
+# Split sentences into words and print first two lines of data
 reviews["Words"] = reviews["Sentences"].apply(
-    lambda sent_list: [sentence_tokenize(s) for s in sent_list]
+    lambda sent_list: [split_sentences_into_words(s) for s in sent_list]
 )
+print("\nAfter Splitting Into Words:")
+print(reviews[0:2])
 
-print("After Word split")
-print(reviews[0:1])
-print("\n\n")
+# Turn words to lowercase and print first few lines
+reviews["Words"] = reviews["Words"].apply(
+    lambda group: [lowercase_words(sentence) for sentence in group]
+)
+print("\nAfter Making All Words Lowercase:")
+print(reviews[0:2])
